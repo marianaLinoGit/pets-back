18a2c2965d00b615ee8138a11417d578727ebe6a
Mariana Lino <mariana.lino.git@gmail.com>
2025-11-05 02:47:23 -0300

    Adicionado tipo de Species em common de schema
    Ajustado em common de openapi

--- src/openapi/components/common.ts
+++ src/openapi/components/common.ts
@@ -1,5 +1,5 @@
 import { z } from "../../lib/z";
 
-export const SpeciesEnum = z.enum(["dog", "cat", "other"]);
+export const SpeciesEnum = z.enum(["dog", "cat", "dog_cat", "other"]);
 export const okArray = z.array(z.any());
 export const okObj = z.any();
--- src/schemas/common.ts
+++ src/schemas/common.ts
@@ -1,5 +1,7 @@
 import { z } from "../lib/z";
 
+export const SpeciesEnum = z.enum(["dog", "cat", "dog_cat", "other"]);
+
 export const ISO_DATE = /^\d{4}-\d{2}-\d{2}$/;
 export const ISO_DATETIME =
 	/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}(:\d{2})?(\.\d+)?(Z|[+\-]\d{2}:\d{2})$/;
